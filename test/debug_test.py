f68c8194af3cecd29f40050512cd59e1ed660dbd
foru120 <broodsky1122@hanmail.net>
2018-01-18 18:32:59 +0900

    2018-01-18, company

--- test/debug_test.py
+++ test/debug_test.py
@@ -31,14 +31,50 @@
 #
 #     print(_str)
 
-import tensorflow as tf
+# import tensorflow as tf
+#
+# x = [[[1,2,3],
+#       [4, 5, 6]
+#      ],
+#      [[6,7,8],
+#       [6,7,8]
+#      ]
+#     ]  # (2, 2, 3)
+# y = tf.reduce_mean(x, (1, 2))
+#
+# with tf.Session() as sess:
+#     sess.run(tf.global_variables_initializer())
+#     print(sess.run(y))
+#     # print(c3.get_shape())
+#     # print(sess.run(c3))
 
-c1 = tf.constant([1, 3, 5, 7, 9, 0, 2, 4, 6, 8])
-c2 = tf.tile(c1, [3])
-c3 = tf.random_normal([10, 0, 1])
+# s = 'asdfqwerszz'
+#
+# def alphabet_counter(text, search):
+#     cnt = 0
+#     for alphabet in text:
+#        if alphabet == search:
+#            cnt += 1
+#     return cnt
+#
+# def convert_word(word, cnt):
+#     return chr(ord(word) + cnt - 26) if (ord(word) + cnt) > ord('z') else chr(ord(word) + cnt)
+#
+# for idx, word in enumerate(s):
+#     print(convert_word(word, alphabet_counter(s[:idx], word)), end='')
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    print(sess.run(c2))
-    print(c3.get_shape())
-    print(sess.run(c3))
+s = 'asdfqwerszz'
+word_count = {}
+
+def convert_word_func(word, cnt):
+    return chr(ord(word) + cnt - 26) if (ord(word) + cnt) > ord('z') else chr(ord(word) + cnt)
+
+for idx, word in enumerate(s):
+    convert_word = ''
+    if word in word_count:
+        convert_word = convert_word_func(word, word_count[word])
+        word_count[word] += 1
+    else:
+        convert_word = word
+        word_count[word] = 1
+    print(convert_word, end='')
