214973a2a80f1b6e75c7b8e5f51fe5f15ef54d2c
foru120 <broodsky1122@hanmail.net>
2018-03-04 12:02:09 +0900

    2018-03-04, company

---
+++ Hongbog/SuperResolution/data_loader.py
@@ -0,0 +1,60 @@
+import time
+import tensorflow as tf
+import numpy as np
+import os
+
+class Dataloader:
+
+    def __init__(self, data_path, batch_size):
+        self.data_path = data_path
+        self.batch_size = batch_size
+
+        stime = time.time()
+        self._data_setting()
+        etime = time.time()
+        print('Data Path Loading Complete >>>', round(etime - stime, 2))
+
+    def _data_setting(self):
+        file_paths = []
+
+        for dirpath, dirnames, filenames in os.walk(self.data_path):
+            if filenames:
+                for filename in filenames:
+                    file_paths.append(os.path.join(dirpath, filename))
+
+        self.tot_cnt = len(file_paths)
+        self.train_paths = file_paths[0: int(self.tot_cnt * 0.6)]
+        self.train_cnt = len(self.train_paths)
+        self.test_paths = file_paths[int(self.tot_cnt * 0.6): int(self.tot_cnt * 0.9)]
+        self.test_cnt = len(self.test_paths)
+        self.valid_paths = file_paths[int(self.tot_cnt * 0.9): ]
+        self.valid_cnt = len(self.valid_paths)
+
+    def _train_loader(self):
+        filename_queue = tf.train.string_input_producer(self.train_paths, shuffle=True)
+        reader = tf.TextLineReader()
+        _, value = reader.read(filename_queue)
+        record_defaults = [[0.] for _ in range(64*48*2)]
+        xy = tf.decode_csv(value, record_defaults=record_defaults)
+        self.train_x, self.train_y = tf.train.batch([xy[0: 64*48], xy[64*48: ]], batch_size=self.batch_size)
+
+    def _test_loader(self):
+        filename_queue = tf.train.string_input_producer(self.test_paths, shuffle=True)
+        reader = tf.TextLineReader()
+        _, value = reader.read(filename_queue)
+        record_defaults = [[0.] for _ in range(64*48*2)]
+        xy = tf.decode_csv(value, record_defaults=record_defaults)
+        self.test_x, self.test_y = tf.train.batch([xy[0: 64*48], xy[64*48: ]], batch_size=self.batch_size)
+
+    def _valid_loader(self):
+        filename_queue = tf.train.string_input_producer(self.valid_paths, shuffle=True)
+        reader = tf.TextLineReader()
+        key, value = reader.read(filename_queue)
+        record_defaults = [[0.] for _ in range(64*48*2)]
+        xy = tf.decode_csv(value, record_defaults=record_defaults)
+        self.valid_x, self.valid_y = tf.train.batch([xy[0: 64*48], xy[64*48: ]], batch_size=self.batch_size)
+
+    def start(self):
+        self._train_loader()
+        self._valid_loader()
+        self._test_loader()
--- Hongbog/SuperResolution/ori_main.py
+++ Hongbog/SuperResolution/ori_main.py
@@ -1,14 +1,15 @@
 import tensorflow as tf
 import numpy as np
+import os
 import time
 from collections import deque
+import cx_Oracle
+import datetime
 
 from PIL import Image
 from PIL import ImageGrab
 
 from Hongbog.SuperResolution.model import Model
-from Hongbog.SuperResolution.data_loader import Dataloader
-from Hongbog.SuperResolution.database import Database
 
 class Neuralnet:
     '''신경망을 훈련하기 위한 클래스'''
@@ -37,10 +38,14 @@ class Neuralnet:
         self.save_type = save_type
         self._flag_setting()  # flag setting
 
-        if (is_train == True) and (save_type == 'db'):  # data loading
-            self.db = Database()
-            self.db.init_database()
-            self.db.get_max_log_num()
+        if is_train == True:  # data loading
+            stime = time.time()
+            self._file_separation()
+            etime = time.time()
+
+            if save_type == 'db':
+                self._init_database()
+                self._get_max_log_num()
 
     def _flag_setting(self):
         '''
@@ -54,7 +59,54 @@ class Neuralnet:
         flags.DEFINE_integer('batch_size', 10, '훈련시 배치 크기')
         flags.DEFINE_integer('max_checks_without_progress', 20, '특정 횟수 만큼 조건이 만족하지 않은 경우')
         flags.DEFINE_string('trained_param_path', 'D:/Source/PythonRepository/Hongbog/SuperResolution/train_log/0002/image_processing_param.ckpt', '훈련된 파라미터 값 저장 경로')
-        flags.DEFINE_integer('data_cnt_per_file', 100, '파일당 데이터 개수')
+        flags.DEFINE_string('mon_data_log_path', 'D:/Source/PythonRepository/Hongbog/SuperResolution//mon_log/mon_' + datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S') + '.txt', '훈련시 모니터링 데이터 저장 경로')
+
+    def _init_database(self):
+        '''
+        데이터베이스 연결을 수행하는 함수
+        :return: None
+        '''
+        self._conn = cx_Oracle.connect('hongbog/hongbog0102@localhost:1521/orcl')
+
+    def _get_cursor(self):
+        '''
+        데이터베이스 커서를 생성하는 함수
+        :return: 데이터베이스 커서, type -> cursor
+        '''
+        return self._conn.cursor()
+
+    def _close_cursor(self, cur):
+        '''
+        데이터베이스 커서를 닫는 함수
+        :param cur: 닫을 커서
+        :return: None
+        '''
+        cur.close()
+
+    def _close_conn(self):
+        '''
+        데이터베이스 연결을 해제하는 함수
+        :return: None
+        '''
+        self._conn.close()
+
+    def _file_separation(self):
+        file_list = np.array(os.listdir(self._FLAGS.blurring_image_path))
+        rand_nums = np.random.permutation(len(file_list))
+        cnt = len(file_list)
+        self._tot_train_file = file_list[rand_nums[:int(cnt * 0.6)]]
+        self._tot_test_file = file_list[rand_nums[int(cnt * 0.6):int(cnt * 0.9)]]
+        self._tot_valid_file = file_list[rand_nums[int(cnt * 0.9):]]
+
+    def _data_loading(self, file_name):
+        '''
+        최종 전처리된 데이터 값을 로딩하는 함수
+        :return: None
+        '''
+        image_size = 64 * 48
+
+        data = np.loadtxt(os.path.join(self._FLAGS.blurring_image_path, file_name), delimiter=',', dtype=np.float32)
+        return data[:, :image_size], data[:, image_size:]
 
     def _get_model_params(self):
         '''
@@ -83,35 +135,59 @@ class Neuralnet:
         im = ImageGrab.grab()
         im.show()
 
+    def _mon_data_to_file(self, train_acc_mon, train_loss_mon, valid_acc_mon, valid_loss_mon):
+        '''
+        모니터링 대상(훈련 정확도, 훈련 손실 값, 검증 정확도, 검증 손실 값) 파일로 저장
+        :param train_acc_mon: 훈련 정확도
+        :param train_loss_mon: 훈련 손실 값
+        :param valid_acc_mon: 검증 정확도
+        :param valid_loss_mon: 검증 손실 값
+        :return: None
+        '''
+        with open(self._FLAGS.mon_data_log_path, 'a') as f:
+            f.write(','.join([str(train_acc_mon), str(valid_acc_mon), str(train_loss_mon), str(valid_loss_mon)]) + '\n')
+
+    def _get_max_log_num(self):
+        '''
+        현재 로깅된 최대 로그 숫자를 DB 에서 가져오는 함수
+        :return: None
+        '''
+        cur = self._get_cursor()
+        cur.execute('select nvl(max(log_num), 0) max_log_num from super_res_log')
+        self._max_log_num = cur.fetchone()[0]
+        self._close_cursor(cur)
+
+    def _mon_data_to_db(self, train_psnr_mon, train_loss_mon, valid_psnr_mon, valid_loss_mon, train_time):
+        '''
+        모니터링 대상(훈련 정확도, 훈련 손실 값, 검증 정확도, 검증 손실 값) DB로 저장
+        :param train_psnr_mon: 훈련 PSNR
+        :param train_loss_mon: 훈련 손실 값
+        :param valid_psnr_mon: 검증 PSNR
+        :param valid_loss_mon: 검증 손실 값
+        :return: None
+        '''
+        cur = self._get_cursor()
+        cur.execute('insert into super_res_log values(:log_num, :log_time, :train_time, :train_psnr, :valid_psnr, :train_loss, :valid_loss)',
+                    [self._max_log_num+1, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), train_time, train_psnr_mon, valid_psnr_mon, train_loss_mon, valid_loss_mon])
+        self._conn.commit()
+        self._close_cursor(cur)
+
     def train(self):
         '''
         신경망을 학습하는 함수
         :return: None
         '''
-        config = tf.ConfigProto(
-            gpu_options=tf.GPUOptions(allow_growth=True, per_process_gpu_memory_fraction=1)
-        )
-
-        with tf.Session(config=config) as sess:
+        with tf.Session() as sess:
             self._model = Model(sess)
 
             sess.run(tf.global_variables_initializer())
             self._saver = tf.train.Saver()
 
-            ck_point = tf.train.get_checkpoint_state(self._FLAGS.trained_param_path)
-
-            if ck_point:
-                self._saver.restore(sess, self._FLAGS.trained_param_path)
-
-            self.loader = Dataloader(data_path='D:\\Data\\casia_blurring\\image_data', batch_size=self._FLAGS.batch_size)
-            self.loader.start()
+            self._saver.restore(sess, self._FLAGS.trained_param_path)
 
             best_loss_val = np.infty  # 가장 좋은 loss 값을 저장하는 변수
             check_since_last_progress = 0  # early stopping 조건을 만족하지 않은 횟수
             self._best_model_params = None  # 가장 좋은 모델의 parameter 값을 저장하는 변수
-
-            coord = tf.train.Coordinator()
-            threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
             print('Train start!!')
 
@@ -124,24 +200,26 @@ class Neuralnet:
                 stime = time.time()
 
                 # 훈련 부분
-                for idx in range(self.loader.train_cnt * int(self._FLAGS.data_cnt_per_file / self._FLAGS.batch_size)):
-                    s_batch_time = time.time()
-                    train_x, train_y = sess.run([self.loader.train_x, self.loader.train_y])
-                    train_psnr, train_loss, _ = self._model.train(train_x.reshape(-1, 64, 48, 1), train_y.reshape(-1, 64, 48, 1))
-                    tot_train_psnr_list.append(train_psnr)
-                    tot_train_loss += train_loss / self._FLAGS.batch_size
-                    e_batch_time = time.time()
-                    print('[Training] [' + str(int(idx % self._FLAGS.batch_size) + 1) + '/10] Epoch: ' + str(epoch) + ', PSNL: ' + str(train_psnr) + ', Loss: ' + str(train_loss) + ', Time(s): ' + str(round(e_batch_time - s_batch_time, 2)))
+                for train_file in self._tot_train_file:
+                    train_x, train_y = self._data_loading(train_file)
+                    for idx in range(0, 100, self._FLAGS.batch_size):
+                        s_batch_time = time.time()
+                        train_psnr, train_loss, _ = self._model.train(train_x[idx:idx+self._FLAGS.batch_size].reshape(-1, 64, 48, 1), train_y[idx:idx+self._FLAGS.batch_size].reshape(-1, 64, 48, 1))
+                        tot_train_psnr_list.append(train_psnr)
+                        tot_train_loss += train_loss / self._FLAGS.batch_size
+                        e_batch_time = time.time()
+                        print('[Training] [' + str(int(idx/self._FLAGS.batch_size) + 1) + '/10] Epoch: ' + str(epoch) + ', PSNL: ' + str(train_psnr) + ', Loss: ' + str(train_loss) + ', Time(s): ' + str(round(e_batch_time - s_batch_time, 2)))
 
                 # 검증 부분
-                for idx in range(self.loader.valid_cnt * int(self._FLAGS.data_cnt_per_file / self._FLAGS.batch_size)):
-                    s_batch_time = time.time()
-                    valid_x, valid_y = sess.run([self.loader.valid_x, self.loader.valid_y])
-                    valid_psnr, valid_loss = self._model.validation(valid_x.reshape(-1, 64, 48, 1), valid_y.reshape(-1, 64, 48, 1))
-                    tot_valid_psnr_list.append(valid_psnr)
-                    tot_valid_loss += valid_loss / self._FLAGS.batch_size
-                    e_batch_time = time.time()
-                    print('[Validation] [' + str(int(idx % self._FLAGS.batch_size) + 1) + '/10] Epoch: ' + str(epoch) + ', PSNL: ' + str(train_psnr) + ', Loss: ' + str(train_loss) + ', Time(s): ' + str(round(e_batch_time - s_batch_time, 2)))
+                for valid_file in self._tot_valid_file:
+                    valid_x, valid_y = self._data_loading(valid_file)
+                    for idx in range(0, 100, self._FLAGS.batch_size):
+                        s_batch_time = time.time()
+                        valid_psnr, valid_loss = self._model.validation(valid_x[idx:idx+self._FLAGS.batch_size].reshape(-1, 64, 48, 1), valid_y[idx:idx+self._FLAGS.batch_size].reshape(-1, 64, 48, 1))
+                        tot_valid_psnr_list.append(valid_psnr)
+                        tot_valid_loss += valid_loss / self._FLAGS.batch_size
+                        e_batch_time = time.time()
+                        print('[Validation] [' + str(int(idx / self._FLAGS.batch_size) + 1) + '/10] Epoch: ' + str(epoch) + ', PSNL: ' + str(valid_psnr) + ', Loss: ' + str(valid_loss) + ', Time(s): ' + str(round(e_batch_time - s_batch_time, 2)))
 
                 etime = time.time()
 
@@ -151,12 +229,14 @@ class Neuralnet:
                 valid_loss_mon = tot_valid_loss
                 train_time = etime - stime
 
-                print('epoch:', epoch + 1, ', train psnr:', round(train_psnr_mon, 2), ', train loss:', round(train_loss_mon, 2), ', validation psnr:', round(valid_psnr_mon, 2), ', validation loss:', round(valid_loss_mon, 2), ', train time:', round(train_time, 2))
+                print('epoch:', epoch + 1, ', train psnr:', round(train_psnr_mon, 2), ', train loss:', round(train_loss_mon, 2),
+                      ', validation psnr:', round(valid_psnr_mon, 2), ', validation loss:', round(valid_loss_mon, 2), ', train time:',
+                      round(train_time, 2))
 
                 if self.save_type == 'file':
-                    self.db.mon_data_to_file(train_psnr_mon, train_loss_mon, valid_psnr_mon, valid_loss_mon)
+                    self._mon_data_to_file(train_psnr_mon, train_loss_mon, valid_psnr_mon, valid_loss_mon)
                 else:
-                    self.db.mon_data_to_db(train_psnr_mon, train_loss_mon, valid_psnr_mon, valid_loss_mon, train_time)
+                    self._mon_data_to_db(train_psnr_mon, train_loss_mon, valid_psnr_mon, valid_loss_mon, train_time)
 
                 self.train_acc_mon.append(train_psnr_mon)
                 self.train_loss_mon.append(train_loss_mon)
@@ -177,14 +257,9 @@ class Neuralnet:
                     print('Early stopping - epoch(' + str(epoch+1) + ')')
                     break
 
-            self._test(sess)
+            self._test()
 
-            coord.request_stop()
-            coord.join(threads)
-
-
-
-    def _test(self, sess):
+    def _test(self):
         '''
         테스트 데이터에 대해 분류를 수행하는 함수
         :return: None
@@ -195,14 +270,15 @@ class Neuralnet:
 
         tot_test_psnr = 0.
 
-        for _ in range(self.loader.test_cnt * int(self._FLAGS.data_cnt_per_file / self._FLAGS.batch_size)):
-            test_x, test_y = sess.run([self.loader.test_x, self.loader.test_y])
-            psnr = self._model.get_psnr(test_x.reshape(-1, 64, 48, 1), test_y.reshape(-1, 64, 48, 1))
-            tot_test_psnr = psnr / self._FLAGS.batch_size
+        for test_file in self._tot_test_file:
+            test_x, test_y = self._data_loading(test_file)
+            for idx in range(0, 100, self._FLAGS.batch_size):
+                psnr = self._model.get_psnr(test_x[idx:idx+self._FLAGS.batch_size].reshape(-1, 64, 48, 1), test_y[idx:idx+self._FLAGS.batch_size].reshape(-1, 64, 48, 1))
+                tot_test_psnr = psnr / self._FLAGS.batch_size
 
         print('test psnr:', tot_test_psnr)
 
-        self.db.close_conn()
+        self._close_conn()
 
     def _create_patch_image(self, ori_img):
         '''
@@ -236,7 +312,7 @@ class Neuralnet:
 
         for init_y in range(0, y_pixel, y_delta):
             for init_x in range(0, x_pixel, x_delta):
-                if pixel_cnt in predict_edges:
+                if img_cnt in predict_edges:
                     for y in range(init_y, init_y + y_delta):
                         for x in range(init_x, init_x + x_delta):
                             new_img.putpixel((x, y), predict_edges[pixel_cnt])
