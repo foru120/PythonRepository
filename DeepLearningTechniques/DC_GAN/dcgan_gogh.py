8c8ab81509ae8a9b19cc3b9e017a6a920b2998bd
foru120 <broodsky1122@hanmail.net>
2018-04-03 14:19:02 +0900

    2018-02-25, company

--- DeepLearningTechniques/DC_GAN/dcgan_gogh.py
+++ DeepLearningTechniques/DC_GAN/dcgan_gogh.py
@@ -1,117 +1,149 @@
 import tensorflow as tf
+import tensorlayer as tl
+from tensorlayer.layers import *
 
 class Generator:
-    def __init__(self, depths=[2048, 1024, 512, 256, 128, 64, 32], s_size=4):
+    def __init__(self, depths, s_size=4):
         self.depths = depths + [3]
         self.s_size = s_size
-        self.reuse = False
+        self.output_size = [(3, 4), (6, 7), (12, 14), (23, 28), (45, 55), (89, 109), (178, 218)]
+        self.w_init = tf.random_normal_initializer(stddev=0.02)
+        self.gamma_init = tf.random_normal_initializer(1., 0.02)
+        self.reuse = None
 
-    def __call__(self, inputs, training=False):
-        self.training = training
+    def __call__(self, inputs, training=False, reuse=False, noise=None):
+        def batch_norm(layer, name):
+            if training:
+                with tf.variable_scope(name_or_scope=name):
+                    return BatchNormLayer(layer, act=tf.nn.relu, is_train=True, gamma_init=self.gamma_init)
+            else:
+                with tf.variable_scope(name_or_scope=name, reuse=True):
+                    return BatchNormLayer(layer, act=tf.nn.relu, is_train=False, gamma_init=self.gamma_init)
 
-        with tf.variable_scope('g', reuse=self.reuse):
-            with tf.variable_scope('reshape'):
-                layer = tf.layers.dense(inputs, self.depths[0] * self.s_size * self.s_size)
-                layer = tf.reshape(layer, [-1, self.s_size, self.s_size, self.depths[0]])
-                layer = tf.nn.elu(tf.layers.batch_normalization(layer, training=self.training), name='layer')
+        def deconvolution(layer, name, idx):
+            with tf.variable_scope(name):
+                if idx == 6:
+                    layer.outputs = tf.add(layer.outputs, noise)
+                    layer = DeConv2d(layer, self.depths[idx], [5, 5], out_size=self.output_size[idx], strides=(2, 2),
+                                     padding='SAME', act=tf.identity, W_init=self.w_init, name='decond2d')
+                else:
+                    layer = DeConv2d(layer, self.depths[idx], [5, 5], out_size=self.output_size[idx], strides=(2, 2),
+                                     padding='SAME', act=tf.identity, W_init=self.w_init, name='decond2d')
+                    layer = batch_norm(layer, 'batchnorm')
+            return layer
+
+        with tf.variable_scope('g', reuse=reuse):
+            with tf.variable_scope('input'):
+                layer = InputLayer(inputs, name='input')
+                layer = DenseLayer(layer, n_units=self.depths[0] * self.output_size[0][0] * self.output_size[0][1],
+                                   W_init=self.w_init, act=tf.identity, name='dense')
+                layer = ReshapeLayer(layer, shape=[-1, self.output_size[0][0], self.output_size[0][1], self.depths[0]], name='reshape')
+                layer = batch_norm(layer, 'batch_norm')
 
             for i in range(1, len(self.depths)):
-                layer = self.deconvolution(layer, 'deconv'+str(i), i)
+                layer = deconvolution(layer, 'deconv'+str(i), i)
 
             with tf.variable_scope('tanh'):
-                layer = tf.tanh(layer, 'layer')
+                layer = tf.nn.tanh(layer.outputs, 'g_output')
 
-        self.reuse = True
-        self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='g')
-        return layer
+        self.variables = tl.layers.get_variables_with_name('g', True, True)
+        # self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='g')
 
-    def deconvolution(self, layer, name, idx):
-        with tf.variable_scope(name):
-            layer = tf.layers.conv2d_transpose(layer, self.depths[idx], [5, 5], strides=(2, 2), padding='SAME')
-            if idx == 4:
-                return layer
-            layer = tf.nn.elu(tf.layers.batch_normalization(layer, training=self.training), name='layer')
         return layer
 
 class Discriminator:
-    def __init__(self, depths=[16, 32, 64, 128, 256, 512, 1024]):
+    def __init__(self, depths):
         self.depths = [3] + depths
-        self.reuse = False
+        self.w_init = tf.random_normal_initializer(stddev=0.02)
+        self.gamma_init = tf.random_normal_initializer(1., 0.02)
 
-    def __call__(self, inputs, training=False, name=''):
+    def __call__(self, inputs, training=False, reuse=False, name=''):
+        def batch_norm(layer, name):
+            if training:
+                with tf.variable_scope(name_or_scope=name):
+                    layer = BatchNormLayer(layer, act=lambda x : tl.act.lrelu(x, 0.2), is_train=True, gamma_init=self.gamma_init)
+                    return layer
+            else:
+                with tf.variable_scope(name_or_scope=name, reuse=True):
+                    layer = BatchNormLayer(layer, act=lambda x : tl.act.lrelu(x, 0.2), is_train=False, gamma_init=self.gamma_init)
+                    return layer
+
         def convolution(layer, name, idx):
             with tf.variable_scope(name):
-                layer = tf.layers.conv2d(layer, self.depths[idx], [5, 5], strides=(2, 2), padding='SAME')
-                layer = tf.nn.elu(tf.layers.batch_normalization(layer, training=training), name='layer')
+                layer = Conv2d(layer, n_filter=self.depths[idx], filter_size=(5, 5), strides=(2, 2), padding='SAME', act=None, W_init=self.w_init, name='conv2d')
+                layer = batch_norm(layer, 'batch_norm')
             return layer
 
-        layer = tf.convert_to_tensor(inputs)
+        with tf.variable_scope('d', reuse=reuse):
+            layer = InputLayer(inputs)
+            layer = Conv2d(layer, n_filter=self.depths[0], filter_size=(5, 5), strides=(2, 2), padding='SAME',
+                           act=lambda x : tl.act.lrelu(x, 0.2), W_init=self.w_init, name='conv2d')
 
-        with tf.name_scope('d' + name), tf.variable_scope('d', reuse=self.reuse):
             for i in range(1, len(self.depths)):
                 layer = convolution(layer, 'conv' + str(i), i)
 
             with tf.variable_scope('classify'):
-                batch_size = layer.get_shape()[0].value
-                reshape = tf.reshape(layer, [batch_size, -1])
-                layer = tf.layers.dense(reshape, 2, name='layer')
+                batch_size = layer.outputs.get_shape()[0].value
+                layer = ReshapeLayer(layer, shape=[batch_size, -1])
+                layer = DenseLayer(layer, n_units=2, W_init=self.w_init, act=tf.nn.softmax, name='d_output')
 
-        self.reuse = True
-        self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='d')
-        return layer
+        self.variables = tl.layers.get_variables_with_name('d', True, True)
+        # self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='d')
+
+        return layer.outputs
 
 class DCGAN:
-    def __init__(self, sess, batch_size=100, s_size=4, z_dim=100, img_size=256, learning_rate=0.0002,
+    def __init__(self, sess, batch_size=100, z_dim=100, img_size=(178, 218), learning_rate=0.0004,
                  g_depths=[1024, 512, 256, 128, 64, 32], d_depths=[16, 32, 64, 128, 256, 512]):
-
         self.sess = sess
         self.batch_size = batch_size
-        self.s_size = s_size  # Generator 의 첫 번째 layer
         self.z_dim = z_dim
         self.img_size = img_size
         self.learning_rate = learning_rate
-        self.g = Generator(depths=g_depths, s_size=self.s_size)
+        self.g = Generator(depths=g_depths)
         self.d = Discriminator(depths=d_depths)
+        self.reuse = False
+        self.training = True
         self._network()
 
     def _network(self):
         with tf.name_scope('initialize_step'):
-            self.randomdata = tf.placeholder(dtype=tf.float32, shape=[self.batch_size, self.z_dim])
-            self.traindata = tf.placeholder(dtype=tf.float32, shape=[self.batch_size, self.img_size, self.img_size, 3])
-            self.training = tf.placeholder(dtype=tf.bool, shape=None, name='training')
+            self.batch_z = tf.placeholder(dtype=tf.float32, shape=[self.batch_size, self.z_dim], name='batch_z')
+            self.noise = tf.placeholder(dtype=tf.float32, shape=[self.batch_size, 96, 128, 32], name='noise')
+            self.train_x = tf.placeholder(dtype=tf.float32, shape=[self.batch_size, self.img_size[0], self.img_size[1], 3], name='train_x')
 
         with tf.name_scope('dcgan_network'):
-            self.generated_img = self.g(self.randomdata, training=self.training)  # batch_size, 256, 256, 3
-            g_outputs = self.d(self.generated_img, training=self.training, name='g')
-            t_outputs = self.d(self.traindata, training=self.training, name='t')
+            self.g_logits = self.g(self.batch_z, training=self.training, reuse=self.reuse, noise=self.noise)
+            self.d1_logits = self.d(self.g_logits, training=self.training, reuse=False, name='g')
+            self.d2_logits = self.d(self.train_x, training=self.training, reuse=True, name='t')
 
-        self.g_losses = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.ones([self.batch_size], dtype=tf.int64), logits=g_outputs))
-        self.d_losses = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.zeros([self.batch_size], dtype=tf.int64), logits=g_outputs)) + \
-                        tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.ones([self.batch_size], dtype=tf.int64), logits=t_outputs))
+        self.g_losses = tl.cost.cross_entropy(output=self.d1_logits, target=tf.ones([self.batch_size], dtype=tf.int64), name='g_losses')
+        self.d1_losses = tl.cost.cross_entropy(output=self.d1_logits, target=tf.zeros([self.batch_size], dtype=tf.int64), name='d1_losses')
+        self.d2_losses = tl.cost.cross_entropy(output=self.d2_logits, target=tf.ones([self.batch_size], dtype=tf.int64), name='d2_losses')
+        self.d_losses = self.d1_losses + self.d2_losses
 
-        g_opt = tf.train.AdamOptimizer(learning_rate=self.learning_rate, beta1=0.5)
-        d_opt = tf.train.AdamOptimizer(learning_rate=self.learning_rate, beta1=0.5)
-        self.g_opt_op = g_opt.minimize(self.g_losses, var_list=self.g.variables)
-        self.d_opt_op = d_opt.minimize(self.d_losses, var_list=self.d.variables)
+        # with tf.variable_scope(name_or_scope='g', reuse=True):
+        #     # print(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES))
+        #     self.weights = tf.get_variable('deconv6/decond2d/decond2d/weights')
 
-        with tf.control_dependencies([self.g_opt_op, self.d_opt_op]):
-            self.op = tf.no_op(name='train')
+        # update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope='update_ops')
+        # with tf.control_dependencies(update_ops):
+        self.g_opt_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate, beta1=0.5).minimize(self.g_losses, var_list=self.g.variables)
+        self.d_opt_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate*2, beta1=0.5).minimize(self.d_losses, var_list=self.d.variables)
 
-    def train(self, traindata, randomdata):
-        return self.sess.run([self.g_losses, self.d_losses, self.op],
-                             feed_dict={self.randomdata: randomdata, self.traindata: traindata, self.training: True})
+        # with tf.control_dependencies([self.g_opt_op, self.d_opt_op]):
+        #     self.op = tf.no_op(name='train')
 
-    def generate(self, randomdata):
-        return self.sess.run(self.generated_img, feed_dict={self.randomdata: randomdata, self.training: False})
+    def g_train(self, batch_z, noise_z):
+        self.reuse = False
+        self.training = True
+        return self.sess.run([self.g_losses, self.g_opt_op], feed_dict={self.batch_z: batch_z, self.noise: noise_z})
 
-    def sample_images(self, row=8, col=8, inputs=None):
-        if inputs is None:
-            inputs = self.randomdata
-        images = self.g(inputs, training=True)
-        images = tf.image.convert_image_dtype(tf.div(tf.add(images, 1.0), 2.0), tf.uint8)
-        images = [image for image in tf.split(images, self.batch_size, axis=0)]
-        rows = []
-        for i in range(row):
-            rows.append(tf.concat(images[col * i + 0:col * i + col], 2))
-        image = tf.concat(rows, 1)
-        return tf.image.encode_jpeg(tf.squeeze(image, [0]))
+    def d_train(self, batch_z, noise_z, train_x):
+        self.training = True
+        return self.sess.run([self.d_losses, self.d_opt_op], feed_dict={self.batch_z: batch_z, self.noise: noise_z, self.train_x: train_x})
+
+    def generate(self, batch_z):
+        self.reuse = True
+        self.training = False
+        return self.sess.run(self.g_logits, self.g_losses, self.d_losses, feed_dict={self.batch_z: batch_z})
--- DeepLearningTechniques/DC_GAN/main_gogh.py
+++ DeepLearningTechniques/DC_GAN/main_gogh.py
@@ -1,166 +1,132 @@
 import time
 
-import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-from PIL import ImageGrab
-from drawnow import drawnow
-from PIL import Image
 import os
+from collections import deque
 
 from DeepLearningTechniques.DC_GAN.dcgan_gogh import DCGAN
+from DeepLearningTechniques.DC_GAN.database import Database
+from DeepLearningTechniques.DC_GAN.dataloader import DataLoader
 
-batch_size = 100
+class Neuralnet:
+    '''하이퍼파라미터 관련 변수'''
+    _FLAGS = None
 
-def read_data():
-    tot_image_data = []
-    for idx, filename in enumerate(os.listdir('gogh_data\\')):
-        im = Image.open('D:\\05_source\\PythonRepository\\DeepLearningTechniques\\DC_GAN\\gogh_data\\' + str(filename))
-        load_img = im.load()
-        temp_data = []
-        for i in range(0, 256):
-            for j in range(0, 256):
-                temp_data.append((np.array(load_img[i, j]) / 255).tolist())
-        tot_image_data.append(temp_data)
-        # if idx >= 99:
-        #     break
-    return np.reshape(np.array(tot_image_data), (-1, 256, 256, 3))
 
-def image_screeshot():
-    im = ImageGrab.grab()
-    im.show()
+    '''훈련시 필요한 변수'''
+    _model = None
+    _saver = None
+    _best_model_params = None
 
-# monitoring 관련 parameter
-value_num = 2
-mon_epoch_list = []
-mon_value_list = [[] for _ in range(value_num)]
-mon_color_list = ['blue', 'yellow', 'red', 'cyan', 'magenta', 'green', 'black']
-mon_label_list = ['g_loss', 'd_loss']
+    '''모니터링 대상 변수'''
+    train_gloss_mon = deque(maxlen=1000)  # deque: queue 의 양방향 삽입, 삭제, 검색이 가능
+    train_dloss_mon = deque(maxlen=1000)
 
-def monitor_train_cost():
-    for cost, color, label in zip(mon_value_list, mon_color_list[0:len(mon_label_list)], mon_label_list):
-        plt.plot(mon_epoch_list, cost, c=color, lw=2, ls="--", marker="o", label=label)
-    plt.title('DC-GAN Loss')
-    plt.legend(loc=1)
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.grid(True)
+    def __init__(self, is_train, save_type=None):
+        self.save_type = save_type
+        self._flag_setting()  # flag setting
 
-# 모든 변수들의 값을 출력하는 함수
-def get_model_params():
-    gvars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
-    return {gvar.op.name: value for gvar, value in zip(gvars, tf.get_default_session().run(gvars))}
+        if (is_train == True) and (save_type == 'db'):  # data loading
+            self.db = Database(FLAGS=self._FLAGS, train_log=1)
+            self.db.init_database()
+            self.loader = DataLoader(batch_size=self._FLAGS.batch_size, train_data_path=self._FLAGS.train_data_path)
 
-# 이전 상태를 restore 하는 함수
-def restore_model_params(model_params):
-    gvar_names = list(model_params.keys())
-    assign_ops = {gvar_name: tf.get_default_graph().get_operation_by_name(gvar_name + '/Assign') for gvar_name in gvar_names}
-    init_values = {gvar_name: assign_op.inputs[1] for gvar_name, assign_op in assign_ops.items()}  # inputs : 해당 operation 의 입력 데이터를 표현하는 objects
-    feed_dict = {init_values[gvar_name]: model_params[gvar_name] for gvar_name in gvar_names}
-    tf.get_default_session().run(assign_ops, feed_dict=feed_dict)
+    def _flag_setting(self):
+        '''
+        하이퍼파라미터 값을 설정하는 함수
+        :return: None
+        '''
+        flags = tf.app.flags
+        self._FLAGS = flags.FLAGS
+        flags.DEFINE_string('train_data_path', 'D:/Data/celeba/img/test', '학습 데이터 경로')
+        flags.DEFINE_integer('epochs', 100, '훈련시 에폭 수')
+        flags.DEFINE_integer('batch_size', 100, '훈련시 배치 크기')
+        flags.DEFINE_integer('max_checks_without_progress', 20, '특정 횟수 만큼 조건이 만족하지 않은 경우(Early Stop Condition)')
+        flags.DEFINE_string('trained_param_path',
+                            'D:/Source/PythonRepository/DeepLearningTechniques/DC_GAN/gogh_train_log',
+                            '훈련된 파라미터 값 저장 경로')
+        self.config = tf.ConfigProto(
+            gpu_options=tf.GPUOptions(allow_growth=True, per_process_gpu_memory_fraction=1)
+        )
 
-def create_image(images, epoch):
-    images = tf.image.convert_image_dtype(tf.div(tf.add(images[:10], 1.0), 2.0), tf.uint8)
-    images = [image for image in tf.split(images, 10, axis=0)]
+    def create_image(self, images, epoch, sess):
+        images = tf.image.convert_image_dtype(tf.div(tf.add(images[np.random.permutation(self._FLAGS.batch_size)[:10]], 1.0), 2.0), tf.uint8)
+        images = [image for image in tf.split(images, 10, axis=0)]
 
-    for idx in range(0, len(images)):
-        if not os.path.isdir('gogh_gen_image\\1th_test\\' + str(epoch)):
-            os.mkdir('gogh_gen_image\\1th_test\\' + str(epoch))
-            tf.write_file('gogh_gen_image\\1th_test\\' + str(epoch) + '\\' + str(idx) + '.jpeg', tf.image.encode_jpeg(tf.squeeze(images[idx], [0])))
+        for idx in range(0, len(images)):
+            if not os.path.isdir('gogh_gen_image\\1th_test\\' + str(epoch)):
+                os.mkdir('gogh_gen_image\\1th_test\\' + str(epoch))
+            with open('gogh_gen_image\\1th_test\\' + str(epoch) + '\\' + str(idx) + '.jpeg', mode='wb') as f:
+                f.write(sess.run(tf.image.encode_jpeg(tf.squeeze(images[idx], [0]))))
 
-    # for idx in range(0, len(images)):
-    #     new_img = Image.new("RGB", (256, 256), "white")
-    #     for i in range(0, new_img.size[0]):
-    #         for j in range(0, new_img.size[1]):
-    #             new_img.putpixel((i, j), (int(images[idx][i][j][0]), int(images[idx][i][j][1]), int(images[idx][i][j][2])))
-    #
-    #     if not os.path.isdir('gogh_gen_image\\1th_test\\' + str(epoch)):
-    #         os.mkdir('gogh_gen_image\\1th_test\\' + str(epoch))
-    #
-    #     new_img.save('gogh_gen_image\\1th_test\\' + str(epoch) + '\\' + str(idx) + '.jpeg')
+    def train(self):
+        num_train = self.loader.train_x_len // self._FLAGS.batch_size
+        train_x = self.loader.train_loader()
+        epoch = 1
 
-def save_image(images, sess, epoch):
-    generated = sess.run(images)
+        with tf.Session(config=self.config) as sess:
+            dcgan = DCGAN(sess=sess, batch_size=self._FLAGS.batch_size)
 
-    with open('gen_image\\3th_test\\' + str(epoch) + '.jpeg', 'wb') as f:
-        f.write(generated)
+            print('>> Tensorflow session built. Variables initialized')
+            sess.run(tf.global_variables_initializer())
 
-config = tf.ConfigProto(
-    gpu_options=tf.GPUOptions(allow_growth=True, per_process_gpu_memory_fraction=0.7)
-)
+            self._saver = tf.train.Saver()
 
-with tf.Session(config=config) as sess:
-    # 시작 시간 체크
-    stime = time.time()
+            ckpt_st = tf.train.get_checkpoint_state(self._FLAGS.trained_param_path)
 
-    m = DCGAN(sess, batch_size=batch_size)
+            if ckpt_st is not None:
+                self._saver.restore(sess, ckpt_st.model_checkpoint_path)
 
-    sess.run(tf.global_variables_initializer())
-    saver = tf.train.Saver()
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+            print('>> Running started')
 
-    best_loss_val = np.infty
-    check_since_last_progress = 0  # early stopping 조건을 만족하지 않은 횟수
-    max_checks_without_progress = 100  # 특정 횟수 만큼 조건이 만족하지 않은 경우
-    best_model_params = None  # 가장 좋은 모델의 parameter 값을 저장하는 변수
-    epoch = 1
+            while True:
+                tot_g_loss, tot_d_loss = 0., 0.
+                sst = time.time()
 
-    print('Data Loading ...')
-    total_x = read_data()
-    print('Data Loaded!!!')
+                for step in range(num_train):
+                    st = time.time()
+                    batch_z = np.random.uniform(-1, 1, (self._FLAGS.batch_size, 100))
+                    noise_z = np.random.randn(self._FLAGS.batch_size, 96, 128, 32)
+                    train_data = sess.run(train_x)
 
-    print('Learning Started!')
+                    step_d_loss = 0.
+                    for _ in range(2):
+                        d_loss, _ = dcgan.d_train(batch_z, noise_z, train_data)
+                        tot_d_loss += d_loss
+                        step_d_loss += d_loss
 
-    while True:
-        epoch_stime = time.time()
-        g_tot_loss, d_tot_loss = 0., 0.
+                    step_g_loss = 0.
+                    for _ in range(1):
+                        g_loss, _ = dcgan.g_train(batch_z, noise_z)
+                        tot_g_loss += g_loss
+                        step_g_loss += g_loss
 
-        '''train part'''
-        for start_idx in range(0, 800, batch_size):
-            g_loss, d_loss, *_ = m.train(total_x[start_idx: start_idx+batch_size], np.random.randn(batch_size, 100))
-            g_tot_loss += g_loss / batch_size
-            d_tot_loss += d_loss / batch_size
+                    step_g_loss = step_g_loss / 1
+                    step_d_loss = step_d_loss / 2
+                    et = time.time()
 
-        # monitoring factors
-        mon_epoch_list.append(epoch)
-        mon_value_list[0].append(g_tot_loss)
-        mon_value_list[1].append(d_tot_loss)
+                    print(">> [Training] epoch/step: [%d/%d], g_loss: %.6f, d_loss: %.6f, Step_Time: %.2f" % (
+                        epoch, step, step_g_loss, step_d_loss, et-st))
 
-        epoch_etime = time.time()
-        print('epoch :', epoch, ', g_loss :', round(g_tot_loss, 8), ', d_loss :', round(d_tot_loss, 8), ', time :',
-              round(epoch_etime - epoch_stime, 6))
-        drawnow(monitor_train_cost)
+                eet = time.time()
+                self.db.mon_data_to_db(epoch, tot_g_loss, tot_d_loss, eet-sst)
 
-        if epoch % 100 == 0:
-            create_image(m.generate(np.random.randn(batch_size, 100)), epoch)
-            # save_image(m.sample_images(), sess, epoch)
+                if epoch % 10 == 0:
+                    os.makedirs(os.path.join('gogh_train_log', str(epoch).zfill(6)), exist_ok=True)
+                    self._saver.save(sess, os.path.join(self._FLAGS.trained_param_path, str(epoch).zfill(6), 'image_processing_param.ckpt'))
+                    img = dcgan.generate(np.random.uniform(-1, 1, (self._FLAGS.batch_size, 100)))
+                    self.create_image(img, epoch, sess)
+                    print('>> [Model & Image Saved] epoch: %d' % (epoch))
 
-        '''early stopping condition check'''
-        if d_tot_loss < best_loss_val:
-            best_loss_val = d_tot_loss
-            check_since_last_progress = 0
-            best_model_params = get_model_params()
-            saver.save(sess, 'gogh_train_log/dcgan_v1.ckpt')
-        else:
-            check_since_last_progress += 1
+                epoch += 1
 
-        # if check_since_last_progress > max_checks_without_progress:
-        #     print('Early stopping!')
-        #     break
+            coord.request_stop()
+            coord.join(threads)
 
-        epoch += 1
+            self.db.close_conn()
 
-    print('Learning Finished!')
-
-    # 종료 시간 체크
-    etime = time.time()
-    print('consumption time : ', round(etime-stime, 6))
-
-    print('\nGenerating Started!')
-
-    if best_model_params:
-        restore_model_params(best_model_params)
-
-    create_image(m.generate(np.random.randn(batch_size, 100)), 0)
-    # save_image(m.sample_images(), sess, 0)
-
-    print('Generating Finished!')
+neuralnet = Neuralnet(is_train=True, save_type='db')
+neuralnet.train()
