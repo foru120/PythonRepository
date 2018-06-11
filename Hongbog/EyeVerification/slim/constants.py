262ded068bce5a8dd28eb811a870d42499ea80cd
foru120 <broodsky1122@hanmail.net>
2018-06-11 10:31:29 +0900

    2018-02-25, company

--- Hongbog/EyeVerification/slim/constants.py
+++ Hongbog/EyeVerification/slim/constants.py
@@ -25,3 +25,18 @@ flags.DEFINE_integer('batch_size',
 flags.DEFINE_float('learning_rate',
                    0.001,
                    '훈련 시 Learning Rate')
+
+flags.DEFINE_float('dropout_rate',
+                   0.6,
+                   '훈련 시 Dropout Rate')
+
+flags.DEFINE_integer('hidden_num',
+                     30,
+                     'Residual Block Default FeatureMap 개수')
+
+flags.DEFINE_float('regularizer_scale',
+                   0.0005,
+                   'L2 Regularizer Scale')
+
+# from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
+# print_tensors_in_checkpoint_file(file_name='D:\\Source\\PythonRepository\\Hongbog\\EyeVerification\\slim\\train_log\\002\\right\\model.ckpt-3080', tensor_name='', all_tensors=True)
--- Hongbog/EyeVerification/slim/main.py
+++ Hongbog/EyeVerification/slim/main.py
@@ -3,112 +3,138 @@ from Hongbog.EyeVerification.slim.dataloader import TFRecordDataset
 from Hongbog.EyeVerification.slim.constants import *
 
 import tensorflow.contrib.slim as slim
-from tensorflow.contrib.slim.python.slim.learning import train_step
+import os
 
-class NeuralNet:
+class Trainer(object):
     LOW_IMG_SIZE = (60, 160)
     MID_IMG_SIZE = (80, 200)
     HIGH_IMG_SIZE = (100, 240)
 
-    def __init__(self, is_train, is_logging):
-        if is_train:
-            self._create_dataset()
+    def __init__(self, orientation, dataset_name, num_classes):
+        self.orientation = orientation
+        self.dataset_name = dataset_name
+        self.num_classes = num_classes
 
-    def _create_dataset(self):
-        """Train Dataset Initialization"""
-        tr_low_right_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.train_right_data_dir, dataset_name='eye', num_classes=7, split_name='right_train')
-        tr_mid_right_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.train_right_data_dir, dataset_name='eye', num_classes=7, split_name='right_train')
-        tr_high_right_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.train_right_data_dir, dataset_name='eye', num_classes=7, split_name='right_train')
+        self.log_dir = 'D:\\Source\\PythonRepository\\Hongbog\\EyeVerification\\slim\\train_log\\002\\{}'.format(orientation)
+        self.eval_dir = 'D:\\Source\\PythonRepository\\Hongbog\\EyeVerification\\slim\\train_log\\002_eval\\{}'.format(orientation)
 
-        self.tr_low_right_imgs, self.tr_low_right_labels, self.tr_tot_nums = \
-            tr_low_right_dataset.load_batch(size=NeuralNet.LOW_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=7, scope='tr_low_right_batch')
-        self.tr_mid_right_imgs, self.tr_mid_right_labels, _ = \
-            tr_mid_right_dataset.load_batch(size=NeuralNet.MID_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=7, scope='tr_mid_right_batch')
-        self.tr_high_right_imgs, self.tr_high_right_labels, _ = \
-            tr_high_right_dataset.load_batch(size=NeuralNet.HIGH_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=7, scope='tr_high_right_batch')
+    def _train_dataset(self):
+        tr_low_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.train_right_data_dir if self.orientation == 'right' else flags.FLAGS.train_left_data_dir,
+                                         dataset_name=self.dataset_name, num_classes=self.num_classes, split_name='{}_train'.format(self.orientation))
+        tr_mid_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.train_right_data_dir if self.orientation == 'right' else flags.FLAGS.train_left_data_dir,
+                                         dataset_name=self.dataset_name, num_classes=self.num_classes, split_name='{}_train'.format(self.orientation))
+        tr_high_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.train_right_data_dir if self.orientation == 'right' else flags.FLAGS.train_left_data_dir,
+                                          dataset_name=self.dataset_name, num_classes=self.num_classes, split_name='{}_train'.format(self.orientation))
 
-        tr_low_left_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.train_left_data_dir, dataset_name='eye', num_classes=7, split_name='left_train')
-        tr_mid_left_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.train_left_data_dir, dataset_name='eye', num_classes=7, split_name='left_train')
-        tr_high_left_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.train_left_data_dir, dataset_name='eye', num_classes=7, split_name='left_train')
+        self.tr_low_imgs, self.tr_low_labels, self.tr_tot_nums = \
+            tr_low_dataset.load_batch(size=Trainer.LOW_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=self.num_classes, scope='tr_low_batch')
+        self.tr_mid_imgs, self.tr_mid_labels, _ = \
+            tr_mid_dataset.load_batch(size=Trainer.MID_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=self.num_classes, scope='tr_mid_batch')
+        self.tr_high_imgs, self.tr_high_labels, _ = \
+            tr_high_dataset.load_batch(size=Trainer.HIGH_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=self.num_classes, scope='tr_high_batch')
 
-        self.tr_low_left_imgs, self.tr_low_left_labels, _ = \
-            tr_low_left_dataset.load_batch(size=NeuralNet.LOW_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=7, scope='tr_low_left_batch')
-        self.tr_mid_left_imgs, self.tr_mid_left_labels, _ = \
-            tr_mid_left_dataset.load_batch(size=NeuralNet.MID_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=7, scope='tr_mid_left_batch')
-        self.tr_high_left_imgs, self.tr_high_left_labels, _ = \
-            tr_high_left_dataset.load_batch(size=NeuralNet.HIGH_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=7, scope='tr_high_left_batch')
         self.tr_step_num = self.tr_tot_nums // flags.FLAGS.batch_size
+        print('>>> TFRecord Train Dataset Initialization Complete!')
 
-        """Test Dataset Initialization"""
-        ts_low_right_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.test_right_data_dir, dataset_name='eye', num_classes=7, split_name='right_test')
-        ts_mid_right_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.test_right_data_dir, dataset_name='eye', num_classes=7, split_name='right_test')
-        ts_high_right_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.test_right_data_dir, dataset_name='eye', num_classes=7, split_name='right_test')
+    def _validation_dataset(self):
+        ts_low_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.test_right_data_dir if self.orientation == 'right' else flags.FLAGS.test_left_data_dir,
+                                         dataset_name=self.dataset_name, num_classes=self.num_classes, split_name='{}_test'.format(self.orientation))
+        ts_mid_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.test_right_data_dir if self.orientation == 'right' else flags.FLAGS.test_left_data_dir,
+                                         dataset_name=self.dataset_name, num_classes=self.num_classes, split_name='{}_test'.format(self.orientation))
+        ts_high_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.test_right_data_dir if self.orientation == 'right' else flags.FLAGS.test_left_data_dir,
+                                          dataset_name=self.dataset_name, num_classes=self.num_classes, split_name='{}_test'.format(self.orientation))
 
-        self.ts_low_right_imgs, self.ts_low_right_labels, self.ts_tot_nums = \
-            ts_low_right_dataset.load_batch(size=NeuralNet.LOW_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=7, scope='ts_low_right_batch')
-        self.ts_mid_right_imgs, self.ts_mid_right_labels, _ = \
-            ts_mid_right_dataset.load_batch(size=NeuralNet.MID_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=7, scope='ts_mid_right_batch')
-        self.ts_high_right_imgs, self.ts_high_right_labels, _ = \
-            ts_high_right_dataset.load_batch(size=NeuralNet.HIGH_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=7, scope='ts_high_right_batch')
+        self.ts_low_imgs, self.ts_low_labels, self.ts_tot_nums = \
+            ts_low_dataset.load_batch(size=Trainer.LOW_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=self.num_classes, scope='ts_low_batch')
+        self.ts_mid_imgs, self.ts_mid_labels, _ = \
+            ts_mid_dataset.load_batch(size=Trainer.MID_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=self.num_classes, scope='ts_mid_batch')
+        self.ts_high_imgs, self.ts_high_labels, _ = \
+            ts_high_dataset.load_batch(size=Trainer.HIGH_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=self.num_classes, scope='ts_high_batch')
 
-        ts_low_left_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.test_left_data_dir, dataset_name='eye', num_classes=7, split_name='left_test')
-        ts_mid_left_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.test_left_data_dir, dataset_name='eye', num_classes=7, split_name='left_test')
-        ts_high_left_dataset = TFRecordDataset(tfrecord_dir=flags.FLAGS.test_left_data_dir, dataset_name='eye', num_classes=7, split_name='left_test')
-
-        self.ts_low_left_imgs, self.ts_low_left_labels, _ = \
-            ts_low_left_dataset.load_batch(size=NeuralNet.LOW_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=7, scope='ts_low_left_batch')
-        self.ts_mid_left_imgs, self.ts_mid_left_labels, _ = \
-            ts_mid_left_dataset.load_batch(size=NeuralNet.MID_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=7, scope='ts_mid_left_batch')
-        self.ts_high_left_imgs, self.ts_high_left_labels, _ = \
-            ts_high_left_dataset.load_batch(size=NeuralNet.HIGH_IMG_SIZE, batch_size=flags.FLAGS.batch_size, num_classes=7, scope='ts_high_left_batch')
         self.ts_step_num = self.ts_tot_nums // flags.FLAGS.batch_size
+        print('>>> TFRecord Validation Dataset Initialization Complete!')
 
-        print('>>> TFRecord Dataset Initialization Complete!')
+    def train(self, epoch):
+        with tf.variable_scope(name_or_scope=self.orientation):
+            tf.logging.set_verbosity(tf.logging.INFO)
 
-    def train(self):
-        tf.logging.set_verbosity(tf.logging.INFO)
+            """Train Model Part"""
+            self._train_dataset()
 
-        """Train Model Part"""
-        right_model = Model(low_res_inputs=self.tr_low_right_imgs, mid_res_inputs=self.tr_mid_right_imgs,
-                            high_res_inputs=self.tr_high_right_imgs, name='right_model')
-        tr_right_logits = right_model.logits
-        tr_right_loss = slim.losses.softmax_cross_entropy(logits=tr_right_logits, onehot_labels=self.tr_low_right_labels, scope='tr_right_ce_loss')
-        tr_right_tot_loss = tf.add_n([tr_right_loss] +
-                                     [var for var in tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES) if 'right_model' in var.name], name='tr_right_tot_loss')
+            tr_model = Model(low_res_inputs=self.tr_low_imgs, mid_res_inputs=self.tr_mid_imgs, high_res_inputs=self.tr_high_imgs)
+            tr_logits = tr_model.logits
+            tr_loss = slim.losses.softmax_cross_entropy(logits=tr_logits, onehot_labels=self.tr_low_labels, scope='tr_ce_loss')
+            tr_tot_loss = tf.add_n([tr_loss] +
+                                   [var for var in tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES) if self.orientation in var.name], name='tr_tot_loss')
 
-        right_opt = tf.train.GradientDescentOptimizer(learning_rate=flags.FLAGS.learning_rate, name='right_opt')
+            opt = tf.train.GradientDescentOptimizer(learning_rate=flags.FLAGS.learning_rate, name='opt')
 
-        right_train_op = slim.learning.create_train_op(
-            total_loss=tr_right_tot_loss,
-            optimizer=right_opt,
-            variables_to_train= [var for var in tf.trainable_variables() if 'right_model' in var.name]
-        )
+            """텐서보드 시각화를 위한 훈련 메트릭 정의"""
+            accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.arg_max(tr_logits, 1), tf.arg_max(self.tr_low_labels, 1)), tf.float32))
+            tf.summary.scalar('losses/Total', tr_tot_loss)
+            tf.summary.scalar('accuracy', accuracy)
+            summary_op = tf.summary.merge_all()
 
-        slim.learning.train(train_op=right_train_op,
-                            logdir='D:\\Source\\PythonRepository\\Hongbog\\EyeVerification\\slim\\train_log\\test\\right',
-                            number_of_steps=5,
-                            save_summaries_secs=300,
-                            save_interval_secs=1)
+            """훈련 수행"""
+            os.makedirs(self.log_dir, exist_ok=True)
+            # update_opt = [var for var in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES) if ('moving_mean' in var.name)
+            #               or ('moving_variance' in var.name)]
+            # update_opt = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 
-        left_model = Model(low_res_inputs=self.tr_low_left_imgs, mid_res_inputs=self.tr_mid_left_imgs,
-                           high_res_inputs=self.tr_high_left_imgs, name='left_model')
-        tr_left_logits = left_model.logits
-        tr_left_loss = slim.losses.softmax_cross_entropy(logits=tr_left_logits, onehot_labels=self.tr_low_left_labels, scope='tr_left_ce_loss')
-        tr_left_tot_loss = slim.losses.get_total_loss()
+            train_op = slim.learning.create_train_op(
+                total_loss=tr_tot_loss,
+                # update_ops=None,  # update_ops = [] : 모든 update_ops 를 사용하지 않도록 설정
+                optimizer=opt,
+                variables_to_train= [var for var in tf.trainable_variables() if self.orientation in var.name]
+            )
 
-        left_opt = tf.train.GradientDescentOptimizer(learning_rate=flags.FLAGS.learning_rate, name='left_opt')
+            slim.learning.train(
+                train_op=train_op,
+                logdir=self.log_dir,
+                number_of_steps=self.tr_step_num * epoch,
+                summary_op=summary_op,
+                save_summaries_secs=60,
+                save_interval_secs=60
+            )
 
-        left_train_op = slim.learning.create_train_op(
-            total_loss=tr_left_tot_loss,
-            optimizer=left_opt,
-            variables_to_train=[var for var in tf.trainable_variables() if 'left_model' in var.name]
-        )
+    def validation(self):
+        with tf.variable_scope(name_or_scope=self.orientation):
+            tf.logging.set_verbosity(tf.logging.INFO)
 
-        slim.learning.train(train_op=left_train_op,
-                            logdir='D:\\Source\\PythonRepository\\Hongbog\\EyeVerification\\slim\\train_log\\test\\left',
-                            number_of_steps=5,
-                            save_summaries_secs=300,
-                            save_interval_secs=1)
+            """Validation Model Part"""
+            self._validation_dataset()
 
-neural_net = NeuralNet(is_train=True, is_logging=True)
-neural_net.train()
+            ts_model = Model(low_res_inputs=self.ts_low_imgs, mid_res_inputs=self.ts_mid_imgs, high_res_inputs=self.ts_high_imgs, is_training=False)
+            ts_logit = ts_model.logits
+
+            "평가 메트릭 정의"
+            names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
+                'eval/Accuracy': slim.metrics.streaming_accuracy(tf.arg_max(ts_logit, 1), tf.arg_max(self.ts_low_labels, 1))
+            })
+
+            """평가 수행"""
+            init_op = tf.group(
+                tf.global_variables_initializer(),
+                tf.local_variables_initializer()
+            )
+
+            checkpoint_path = tf.train.latest_checkpoint(self.log_dir)
+
+            slim.get_or_create_global_step()
+            metric_values = slim.evaluation.evaluate_once(
+                master='',
+                checkpoint_path=checkpoint_path,
+                logdir=self.eval_dir,
+                num_evals=self.ts_step_num,
+                initial_op=init_op,
+                eval_op=list(names_to_updates.values()),  # names_to_updates.values() -> type 이 dict_values 이어서 list 로 변환시켜주어야 한다.
+                final_op=list(names_to_values.values())
+            )
+
+            names_to_values = dict(zip(names_to_values.keys(), metric_values))
+            for name in names_to_values:
+                print('%s: %f' % (name, names_to_values[name]))
+
+trainer = Trainer(orientation='right', dataset_name='eye', num_classes=7)
+trainer.validation()
+# trainer.train(epoch=10)
